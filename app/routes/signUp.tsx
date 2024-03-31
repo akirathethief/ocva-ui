49e6216b2ab062d2b787dc52253e9cc8a799bb2c
Akira <76575762+akirathethief@users.noreply.github.com>
2024-03-30 20:12:56 -0700

    updated git.ignore

---
+++ app/routes/signUp.tsx
@@ -0,0 +1,10 @@
+import {Outlet} from "@remix-run/react";
+
+export default function SignUp() {
+    return (
+        <div>
+            <h1>testing</h1>
+            <Outlet />
+        </div>
+    );
+}
